8eed9f2d4ec1da1201197afab0cac0d03158bfbf
jerichosamuels <126803279+jerichosamuels@users.noreply.github.com>
2023-08-17 14:43:58 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `samjbell.com`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
