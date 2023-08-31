ab5695f0130a1f121ef239963452e1e83199f62c
jerichosamuels <126803279+jerichosamuels@users.noreply.github.com>
2023-08-31 16:35:11 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `samjbell.com`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+    plugins:[
+	"gatsby-plugin-image",
+	"gatsby-plugin-sharp",
+    ],
+};
